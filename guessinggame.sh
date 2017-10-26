3798b7632a046db0fc65ea4969e89a4006aa4f99
Sander <sander.arnus@gmail.com>
2017-10-26 20:43:26 +0300

    First working version of script

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,28 +1,15 @@
 #!/usr/bin/env bash
 
-#Ask user how many files are in the current directory
-
 function guess {
-echo "Guess how many files are in current directory:"
-read response
-
-#Promt for user
-
-#If user's incorrect, let know if the guess was low or high, then guess again
-
-
-#If guess was correct then congradulate and end program
-
-#one function, one loop, one if statement
-# between 21 and 49 lines of code
-
+	echo "Guess how many files are in the current directory:"
+	read response
 }
 
 count=$(ls -1 | wc -l)
 while [[ $count -ne $response ]]
 do
 	guess
-	if [[ $resonse -eq $count ]]
+	if [[ $response -eq $count ]]
 	then
 		echo "Congradulations! You guessed correctly, there are $count files in current directory."
 		exit 0
@@ -33,4 +20,3 @@ do
 		echo "Guess was higher than number of files"
 	fi
 done
-#echo "Congradulations! You guessed correctly, there are $count files in current directory."
